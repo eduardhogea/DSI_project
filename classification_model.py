3930e91cdfc7d31522e74880744cafb86cdb35f7
diana07calina <117271880+diana07calina@users.noreply.github.com>
2023-11-26 18:59:58 +0200

    Add two additional models (Logistic Regression and Linear SVM) to classification models in order to allow the comparison of model performances on the newsgroups dataset.

--- classification_model.py
+++ classification_model.py
@@ -1,4 +1,6 @@
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 from feature_extraction import extract_features
 from sklearn.datasets import fetch_20newsgroups
@@ -6,19 +8,29 @@ from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def train_model(documents, labels):
+def train_model(documents, labels, model_type='naive_bayes'):
     # Extract features
     features, _ = extract_features(documents)
 
     # Splitting the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=1)
 
-    # Training the model
-    model = MultinomialNB()
+    # Select and train the model based on the specified type
+    if model_type == "naive_bayes":
+        model = MultinomialNB()
+    elif model_type == "logistic_regression":
+        model = LogisticRegression(max_iter=1000)
+    elif model_type == "linear_svc":
+        model = LinearSVC(max_iter=1000, dual=False)
+    else:
+        raise ValueError("Unsupported model type")
+
+
     model.fit(X_train, y_train)
 
     # Evaluating the model
     predictions = model.predict(X_test)
+    print(f"Result for {model_type}:")
     print(classification_report(y_test, predictions))
 
     return model, X_test, y_test, predictions
@@ -43,5 +55,9 @@ if __name__ == "__main__":
     labels = newsgroups_train.target
     categories = newsgroups_train.target_names
 
-    model, X_test, y_test, predictions = train_model(documents, labels)
-    plot_confusion_matrix(y_test, predictions, categories)
+
+    # Train and evaluate each model
+    for model_type in ["naive_bayes", "logistic_regression", "linear_svc"]:
+        model, X_test, y_test, predictions = train_model(documents, labels, model_type)
+        plot_confusion_matrix(y_test, predictions, categories)
+
