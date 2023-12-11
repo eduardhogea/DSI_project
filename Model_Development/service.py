59cecba5e6b0ff34dcb22ff40f2592b5e0111501
Hogea Eduard-Florin <eduard.hogea00@e-uvt.ro>
2023-12-11 19:58:54 +0200

    bentoml working locally

--- Model_Development/service.py
+++ Model_Development/service.py
@@ -1,7 +1,11 @@
 from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
 import bentoml
+from bentoml.io import JSON, NumpyNdarray
+from pydantic import BaseModel
+import numpy as np
 import os
 
+# Your ZeroShotClassifier definition
 class ZeroShotClassifier:
     def __init__(self, model_name="typeform/distilbert-base-uncased-mnli", save_dir="model"):
         self.model_name = model_name
@@ -17,23 +21,35 @@ class ZeroShotClassifier:
             tokenizer = AutoTokenizer.from_pretrained(self.model_name)
             model.save_pretrained(self.save_dir)
             tokenizer.save_pretrained(self.save_dir)
-
         return pipeline("zero-shot-classification", model=model, tokenizer=tokenizer)
 
     def classify(self, text, candidate_labels):
         return self.classifier(text, candidate_labels)
 
-def save_model(zero_shot_classifier, model_name):
-    bentoml.transformers.save_model(model_name, zero_shot_classifier.classifier.model, zero_shot_classifier.classifier.tokenizer)
+# Load your ZeroShotClassifier instance
+zero_shot_classifier = ZeroShotClassifier()
 
-def load_model(model_name):
-    classifier = bentoml.transformers.load_model(model_name)
-    return classifier
+# Define the Pydantic model for input validation
+class ClassificationInput(BaseModel):
+    text: str
+    candidate_labels: list[str]
 
-if __name__ == "__main__":
-    zero_shot_classifier = ZeroShotClassifier()
+input_spec = JSON(pydantic_model=ClassificationInput)
 
-    save_model(zero_shot_classifier, "my_zero_shot_classifier")
+# Initialize BentoML service
+svc = bentoml.Service("zero_shot_classifier_service", runners=[])
 
-    loaded_classifier = load_model("my_zero_shot_classifier")
-    print("Model loaded successfully.")
+@svc.api(input=input_spec, output=NumpyNdarray())
+def classify(input_data: ClassificationInput) -> np.ndarray:
+    text = str(input_data.text)
+    candidate_labels = input_data.candidate_labels
+
+    # Use the ZeroShotClassifier's classify method for inference
+    result = zero_shot_classifier.classify(text, candidate_labels)
+
+    # Assuming result is a dictionary with keys 'labels' and 'scores'
+    labels = result.get('labels', [])
+    scores = result.get('scores', [])
+    
+    return np.array([labels, scores])
+
